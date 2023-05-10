dd625f99072d3367a3fde8fcc182fb6b83539f4c
robertsl7642 <126097404+robertsl7642@users.noreply.github.com>
2023-05-09 22:58:17 -0400

    Add files via upload

---
+++ P2LAB2_RobertsLester.py
@@ -0,0 +1,24 @@
+#CTI-110
+#P2LAB2 - Simple Statistics
+#Lester Roberts
+#9 MAY 2023
+
+
+
+#Simple Statistics
+#Input 4 floating points
+num1=float(input())
+num2=float(input())
+num3=float(input())
+num4=float(input())
+
+#Calculate
+#Product
+product = num1*num2*num3*num4
+#Average
+average =(num1+num2+num3+num4)/4
+
+#Output
+print(f'{product:.0f} {average:.0f}')
+print(f'{product:.3f} {average:.3f}')
+
